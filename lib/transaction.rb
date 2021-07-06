aedc91bfdb0bc83d9b5d5d35aaf1e9e06915df39
“Jonny <jonny.kelly192@gmail.com>
2021-07-06 10:00:29 +0100

    initializes deposit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction
 
-    attr_reader :balance
+    attr_reader :balance, :deposit
     
-    def initialize(balance: nil)
+    def initialize(balance: nil, deposit: nil)
         @balance = balance
-
+        @deposit = deposit
     end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,5 +8,10 @@ describe Transaction do
           transaction = Transaction.new(balance: 100)
           expect(transaction.balance).to eq 100
         end
+
+        it "initializes with a deposit" do 
+          transaction = Transaction.new(balance: 100, deposit: 200)
+          expect(transaction.deposit).to eq 200
+        end
     end
 end
